6219afc2633d72489277cad433b68cef900de9e8
bagl0025 <bagl0025@umn.edu>
2022-03-11 10:46:17 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,19 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
+// console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+// for(var i = 0; i < enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + i + " index");
+// }
+
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
     // check if player wants to fight
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -46,10 +52,15 @@ var fight = function() {
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             playerMoney = playerMoney - 2;
         }
-        else {
-            fight();
-        }
+        // else {
+        //     fight();
+        // }
+    }
+    else {
+        window.alert("You need to pick a valid option. Try again!");
     }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
